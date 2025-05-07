5b476dfbd417481fad7ace32ad93c4e29f51fac9
mesquitaaph <raphafm.rf@gmail.com>
2025-05-07 20:19:42 -0300

    test: adiciona testes pro caso 2D e o separa os casos em arquivos diferentes

---
+++ test/caso1D.jl
@@ -0,0 +1,40 @@
+function single_run_1D(example)
+  alpha, beta, gamma, a, b, u, u_x, f = examples(example); ne = 2^3
+
+  run_values = RunValues(alpha, beta, gamma, f, u)
+
+  baseType = BaseTypes.linearLagrange
+  base = monta_base(baseType, ne)
+
+  malha = monta_malha_1D_uniforme(ne, base, a, b)
+
+  C, EQoLG, xPTne = solveSys_geral(run_values, malha)
+
+  return C
+end
+
+
+@testset "caso1D.jl" begin
+  correct_result_ex1 = [
+    0.018379439835948527
+    0.03508858369021478
+    0.048430969064432186
+    0.05665739050754408
+    0.057938496919442535
+    0.05033613330919126
+    0.031772978053653186
+  ]
+  @test single_run_1D(1) ≈ correct_result_ex1
+
+
+  correct_result_ex2 = [
+    0.3831328931456573
+    0.7079372764182132
+    0.9249646268234876
+    1.0011744976201078
+    0.9249646268234876
+    0.7079372764182131
+    0.38313289314565724
+  ] 
+  @test single_run_1D(2) ≈ correct_result_ex2
+end
---
+++ test/caso2D.jl
@@ -0,0 +1,54 @@
+function single_run_2D(ruido::Bool = false)
+  α, β, f, u = exemplo1()
+
+  run_values = RunValues(α, β, 0.0, f, u)
+
+  Nx1, Nx2 = 4, 3; ne = Nx1 * Nx2
+
+  baseType = BaseTypes.linearLagrange
+  base = monta_base(baseType, ne)
+
+  a = (0.0, 0.0); b = (1.0, 1.0)
+  malha = monta_malha_2D_uniforme(base, Nx1, Nx2, a ,b)
+  
+  if ruido
+    # Exemplo 2: Adiciona ruído nos nós internos
+    Random.seed!(42) # Define uma semente para reprodutibilidade
+    malha = malha2D_adiciona_ruido(malha)
+  else
+    # Exemplo 1: Malha uniforme de retângulos
+  end
+
+  K = montaK_geral(run_values, malha)
+
+  F = montaF_geral(run_values, malha)
+
+  C = K\F
+
+  return C
+end
+
+
+@testset "caso2D.jl" begin
+  correct_result_ex1 = [
+    0.659197679603509
+    0.9322462987801567
+    0.659197679603509
+    0.6591976796035091
+    0.9322462987801565
+    0.659197679603509
+  ]
+  ruido = false
+  @test single_run_2D(ruido) ≈ correct_result_ex1
+
+  correct_result_ex2 = [
+    0.7065102277637031
+    0.969014464383587
+    0.6584488958892918
+    0.7346980208325475
+    0.8893466262809584
+    0.7082557097977525
+  ]
+  ruido = true
+  @test single_run_2D(ruido) ≈ correct_result_ex2
+end
