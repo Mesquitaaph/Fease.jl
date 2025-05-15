93bf994fa7755cb5f6c18e39e5ec835f9a76b667
mesquitaaph <raphafm.rf@gmail.com>
2025-05-15 01:40:29 -0300

    refactor: remaneja exportacao de funcoes para final da definicao do modulo do pacote

--- src/MyProject.jl
+++ src/MyProject.jl
@@ -1,27 +1,9 @@
 module MyProject
 
-export format_num, measure_func
-
-export Malha, monta_malha_1D_uniforme, monta_malha_2D_uniforme, malha2D_adiciona_ruido
-
-export examples, RunValues
-
-export BaseTypes, LocalBases, monta_base
-
-export solveSys, PHI, PHI_original
-
-export exemplo1
-
-export convergence_test!
-
-export solveSys_geral, montaK_geral, montaF_geral
-
-
 # Dependencias do modulo
 using 
   BenchmarkTools, GaussQuadrature, Plots, BandedMatrices, Printf, 
   DataFrames, Latexify, Statistics, SparseArrays, LinearAlgebra, Random
-
 
 include("utils.jl")
 
@@ -41,5 +23,24 @@ include("convergence.jl")
 
 include("testes/testes_malha.jl")
 include("testes/testes_simulacao.jl")
+
+
+export format_num, measure_func
+
+export Malha, monta_malha_1D_uniforme, monta_malha_2D_uniforme, malha2D_adiciona_ruido
+
+export examples, RunValues
+
+export BaseTypes, LocalBases, monta_base
+
+export solveSys, PHI, PHI_original
+
+export exemplo1
+
+export convergence_test!
+
+export solveSys_geral, montaK_geral, montaF_geral
+
+export dot
 
 end
