da1d58f835ef55641f54d7342be0bb36d61a708c
mesquitaaph <raphafm.rf@gmail.com>
2025-10-22 18:44:16 -0300

    feat: testa nova montagem de malha

--- src/testes/testes_malha.jl
+++ src/testes/testes_malha.jl
@@ -1,162 +1,54 @@
-function test_montaLG()
-  ne = 5
-
+function teste_foco_2D()
   baseType = BaseTypes.linearLagrange
-  base = monta_base(baseType, ne)
-  LG_1D = montaLG(ne, base)
-  println("Teste montagem LG:")
 
-  LG1D = LG_1D == montaLG_geral(5)
-  println("LG 1D: $LG1D")
+  a = (0.0, 0.0)
+  b = (1.0, 1.0)
 
-  LG2D = monta_LG_2D(5, 4) == montaLG_geral(5, 4)
-  return println("LG 2D: $LG2D")
+  Nx1, Nx2 = 4, 3
+  ponto_foco = (0.3, 0.2)
+
+  # malha = monta_malha_2D_uniforme(baseType, Nx1, Nx2, a, b)
+  malha = monta_malha_2D_foco(baseType, Nx1, Nx2, a, b, ponto_foco, 5)
+
+  plot_malha_2D(malha)
+  example = 1
+  run_values = examples_2D(example)
+
+  c = solve_sys_poisson(run_values, malha)
+
+  # plot_solucao_aproximada(c, malha, false)
+
+  return
 end
-# test_montaLG()
 
-function test_montaEQ()
-  ne = 5
+teste_foco_2D()
 
+function convergence_2D()
   baseType = BaseTypes.linearLagrange
-  base = monta_base(baseType, ne)
-  neq = base.neq
-  EQ_1D = montaEQ(ne, neq, base)
-  println("\nTeste montagem EQ:")
 
-  EQ1D = EQ_1D == montaEQ_geral(5)[2]
-  println("EQ 1D: $EQ1D")
-  # display(EQ_1D); display(montaEQ_geral(5))
+  a = (0.0, 0.0)
+  b = (1.0, 1.0)
 
-  EQ2D = true
-  # display(monta_EQ_2D(5, 4)[2] == montaEQ_geral(5, 4)[2])
-  for nx1 in 1:50
-    for nx2 in 1:50
-      if !(monta_EQ_2D(nx1, nx2)[2] == montaEQ_geral(nx1, nx2)[2])
-        EQ2D = false
-      end
-    end
-  end
-  return println("EQ 2D: $EQ2D")
-end
-# test_montaEQ()
-
-function test_ϕ()
-  println("\nTeste da ϕ:")
-  P, W = legendre(10)
-
-  print("ϕ 1D: ")
-  teste_1D = true
-
-  for ξ₁ in P
-    if !(ϕ_1D(ξ₁) == ϕ_geral(ξ₁))
-      teste_1D = false
-      break
-    end
-  end
-  println(teste_1D ? "ok" : "nok")
-
-  print("ϕ 2D: ")
-  teste_2D = true
-
-  for ξ₁ in P
-    for ξ₂ in P
-      # println("$(ϕ_2D(ξ₁, ξ₂)), $(ϕ_geral(ξ₁, ξ₂))")
-      if !(ϕ_2D(ξ₁, ξ₂) == ϕ_geral(ξ₁, ξ₂)[1])
-        teste_2D = false
-        break
-      end
-    end
-  end
-  return println(teste_2D ? "ok\n" : "nok\n")
-end
-# test_ϕ()
-
-function test_∇ϕ()
-  println("Teste da ∇ϕ")
-  P, W = legendre(2)
-
-  print("∇ϕ 1D: ")
-  teste_1D = true
-
-  for ξ₁ in P
-    if !(∇ϕ_1D(ξ₁) == ∇ϕ_geral(ξ₁))
-      teste_1D = false
-      break
-    end
-  end
-  println(teste_1D ? "ok" : "nok")
-
-  print("∇ϕ 2D: ")
-  teste_2D = true
-
-  for ξ₁ in P
-    for ξ₂ in P
-      ∂ϕ_∂ξ₁_2D, ∂ϕ_∂ξ₂_2D = ∇ϕ_2D(ξ₁, ξ₂)
-      ∂ϕ_∂ξ₁_geral, ∂ϕ_∂ξ₂_geral = ∇ϕ_geral(ξ₁, ξ₂)
-      if !(∂ϕ_∂ξ₁_2D == ∂ϕ_∂ξ₁_geral || ∂ϕ_∂ξ₂_2D == ∂ϕ_∂ξ₂_geral)
-        teste_2D = false
-        break
-      end
-    end
-  end
-  return println(teste_2D ? "ok\n" : "nok\n")
-end
-# test_∇ϕ()
-
-function test_avalia_quadratura_∇ϕ()
-  function avaliar_quadratura_geral_ϕ(
-      base_func::Function, npg::Int64, n_funcs::Int64, n_dim::Int64)
-    P, W = quadratura_gauss(npg, n_dim)
-
-    ϕPₙ = ()
-    for d in 1:n_dim
-      ϕP = zeros(npg^n_dim, n_funcs^n_dim)
-      for ξ in 1:(npg^n_dim)
-        ϕP[ξ, :] .= base_func(P[ξ]...)[1]
-      end
-      ϕPₙ = (ϕPₙ..., ϕP)
-    end
-
-    return ϕPₙ, P, W
+  function monta_malha(NX)
+    ponto_foco = (0.7, 0.5)
+    return monta_malha_2D_foco(baseType, NX..., a, b, ponto_foco, 3)
+    # return monta_malha_2D_uniforme(baseType, NX..., a, b)
   end
 
-  function avaliar_quadratura_geral(
-      base_func::Function, npg::Int64, n_funcs::Int64, n_dim::Int64)
-    P, W = quadratura_gauss(npg, n_dim)
+  example = 1
+  run_values = examples_2D(example)
 
-    ϕPₙ = ()
-    for d in 1:n_dim
-      ϕP = zeros(npg^n_dim, n_funcs^n_dim)
-      for ξ in 1:(npg^n_dim)
-        ϕP[ξ, :] .= base_func(P[ξ]...)[d]
-      end
-      ϕPₙ = (ϕPₙ..., ϕP)
-    end
+  errsize = 7
+  NE = 2 .^ [2:1:errsize;]
+  H = 1 ./ NE
+  E = zeros(length(NE))
+  dE = similar(E)
 
-    return ϕPₙ, P, W
-  end
+  convergence_test!(E, NE, run_values, 2, monta_malha)
 
-  println("Teste da avalia_quadratura para ∇ϕ")
-  npg = 5
-
-  print("ϕ 1D: ")
-  n_dim = 1
-  ∇ϕξ_1D, P_1D, W_1D = avaliar_quadratura_geral(∇ϕ_1D, npg, 2, n_dim)
-  ∇ϕξ_geral, P_geral, W_geral = avaliar_quadratura_geral(∇ϕ_geral, npg, 2, n_dim)
-
-  # display(∇ϕξ_1D)
-  # display(∇ϕξ_geral)
-  display(∇ϕξ_1D == ∇ϕξ_geral)
-
-  print("ϕ 2D: ")
-  n_dim = 2
-  ∇ϕξ_2D, P_2D, W_2D = avaliar_quadratura_geral(∇ϕ_2D, npg, 2, n_dim)
-  ∇ϕξ_geral, P_geral, W_geral = avaliar_quadratura_geral(∇ϕ_geral, npg, 2, n_dim)
-
-  # display(∇ϕξ_2D)
-  # display(∇ϕξ_geral)
-  return display(∇ϕξ_2D == ∇ϕξ_geral)
-  # display(P_geral)
+  H = 1 ./ NE
+  plot(H, E, xaxis = :log10, yaxis = :log10)
+  return plot!(H, H .^ monta_base(baseType, 2).nB, xaxis = :log10, yaxis = :log10)
 end
 
-# test_avalia_quadratura_∇ϕ()
+convergence_2D()
