f77eac1599a3e2fd242bc2a44595c8ae1e055891
mesquitaaph <raphafm.rf@gmail.com>
2025-10-22 16:01:18 -0300

    feat: comenta includes de teste

--- src/testes/include_testes.jl
+++ src/testes/include_testes.jl
@@ -1,4 +1,4 @@
-include("testes_simulacao.jl")
-include("testes_malha.jl")
+#include("testes_simulacao.jl")
+#include("testes_malha.jl")
 # include("testes_parcelas.jl")
-# include("testes_convergencia.jl")
+#include("testes_convergencia.jl")
