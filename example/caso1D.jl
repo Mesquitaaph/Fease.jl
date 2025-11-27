879b8e5e9e8276b021b43d7eb0e40dd106c4ac8b
mesquitaaph <raphafm.rf@gmail.com>
2025-11-27 20:49:45 -0300

    refactor: reorganiza o exemplo do caso 1D

--- example/caso1D.jl
+++ example/caso1D.jl
@@ -1,50 +1,63 @@
 using Fease
 
-ne = 2^3
+function caso_1D()
+  # Define o número de sub-intervalos no eixo horizontal e vertical, respectivamente.
+  Nx1 = 2^3
 
-baseType = BaseTypes.linearLagrange
-
-a, b = 0, 1
-
-malha = monta_malha_1D_uniforme(baseType, ne, a, b)
-
-example = 1
-run_values = examples_1D(example)
-
-(; α, β, f) = run_values
-
-function pseudo_a(termos_equacao)
-  (; ∇u, ∇v, u, v) = termos_equacao
-
-  return β * dot(u, v) + α * dot(∇u, ∇v)
-end
-
-C = solve_sys(f, malha, pseudo_a)
-
-display(C)
-
-function convergence_1D()
+  # Define o tipo da base de funções interpoladoras do subespaço aproximado Vₘ.
   baseType = BaseTypes.linearLagrange
 
+  # Define extremos do intervalo da malha.
   a, b = 0, 1
 
+  # Define a malha com os valores atribuídos acima.
+  malha = monta_malha_1D_uniforme(baseType, Nx1, a, b)
+
+  # Define os parâmetros da equação a ser resolvida.
+  example = 1
+  run_values = examples_1D(example)
+  (; α, β, f) = run_values
+
+  # Define o pseudo operador linear a(u,v).
+  function pseudo_a(termos_equacao)
+    (; ∇u, ∇v, u, v) = termos_equacao
+
+    return β * dot(u, v) + α * dot(∇u, ∇v)
+  end
+
+  # Monta e resolve o sistema linear relacionado a esta equação.
+  c = solve_sys(f, malha, pseudo_a)
+
+  # Imprime solução aproximada.
+  show(c)
+
+  # Plota solução aproximada.
+  plot_solucao_aproximada(c, malha, false)
+
+  # Estudo da convergencia do erro
+
+  # Define função que constrói uma malha que depende só do número de sub-intervalos nos dois eixos.
   function monta_malha(NX)
     return monta_malha_1D_uniforme(baseType, NX[1], a, b)
   end
 
-  example = 1
-  run_values = examples_1D(example)
-
-  errsize = 5
+  # Estudo será realizado com número de elementos em potências de 2.
+  errsize = 7
   NE = 2 .^ [2:1:errsize;]
   H = 1 ./ NE
   E = zeros(length(NE))
-  dE = similar(E)
-  convergence_test!(E, NE, run_values, 1, monta_malha)
 
-  H = 1 ./ NE
-  plot(H, E, xaxis = :log10, yaxis = :log10)
-  return plot!(H, H .^ monta_base(baseType, 2).nB, xaxis = :log10, yaxis = :log10)
+  # Define a solução analítica da equação.
+  (; u) = run_values
+
+  # Calcula o erro para cada uma das quantidades de elementos finitos.
+  n_dim = 1
+  convergence_test!(E, NE, n_dim, monta_malha, pseudo_a, f, u)
+
+  # Plota o resultado do estudo
+  plot(H, E, xaxis = :log10, yaxis = :log10, label = "Erro")
+  return plot!(H, H .^ monta_base(baseType, 2).nB, xaxis = :log10,
+    yaxis = :log10, label = "H²")
 end
 
-convergence_1D()
+caso_1D()
