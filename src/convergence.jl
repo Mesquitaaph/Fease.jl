94d9617966882f756798b41ce612b03c8f87cf3c
mesquitaaph <raphafm.rf@gmail.com>
2025-11-27 20:18:23 -0300

    refactor: torna convergence_test! independente de run_values

--- src/convergence.jl
+++ src/convergence.jl
@@ -45,15 +45,68 @@ function erro_L2(malha::Malha, u, C)
   return EL2, EH01
 end
 
-function convergence_test!(E, NE, run_values, n_dims, monta_malha)
+"""
+    convergence_test!(E, NE, n_dims, monta_malha, pseudo_a, f, u)
+
+Realiza o estudo da convergência do erro para a solução aproximada da equação com solução exata `u`.
+
+# Parâmetros
+- `E::Vetor{Float64}`: O vetor onde serão inseridos os erros caculados para cada um das quantidades de elementos finitos.
+- `NE::Vetor{Float64}`: O vetor que contém as cada uma das quantidades de elementos finitos.
+- `n_dim::Int`: Número de dimensões do espaço.
+- `monta_malha::Function`: Uma função que recebe os números de sub-intervalos da malha e a retorna montada.
+- `pseudo_a`: A referência ao operador bilinear a(u,v).
+- `f::Function`: Função que representa o lado direito da equação do problema.
+- `u::Function`: A solução analítica da equação.
+
+# Retorno
+Altera o vetor `E`.
+
+# Exemplo
+```@example
+  # Define o tipo da base de funções interpoladoras do subespaço aproximado Vₘ.
+  baseType = BaseTypes.linearLagrange
+
+  # Define extremos do intervalo da malha.
+  a = (0.0, 0.0)
+  b = (1.0, 1.0)
+
+  # Define os parâmetros da equação a ser resolvida.
+  α = 1.0
+  β = 1.0
+  f = (x₁, x₂) -> (2 * α * π^2 + β) * sin(π * x₁) * sin(π * x₂)
+  u = (x₁, x₂) -> sin(π * x₁) * sin(π * x₂)
+
+  # Define o pseudo operador linear a(u,v).
+  function pseudo_a(termos_equacao)
+    (; ∇u, ∇v, u, v) = termos_equacao
+
+    return β * dot(u, v) + α * dot(∇u, ∇v)
+  end
+
+  # Define a função que constrói uma malha que depende só do número de sub-intervalos nos dois eixos.
+  function monta_malha(NX)
+    return monta_malha_2D_uniforme(baseType, NX..., a, b)
+  end
+
+  # Estudo será realizado com número de elementos em potências de 2.
+  errsize = 7
+  NE = 2 .^ [2:1:errsize;]
+  H = 1 ./ NE
+  E = zeros(length(NE))
+
+  # Calcula o erro para cada uma das quantidades de elementos finitos.
+  convergence_test!(E, NE, 2, monta_malha, pseudo_a, f, u)
+```
+"""
+function convergence_test!(E, NE, n_dim, monta_malha, pseudo_a, f, u)
   fill!(E, 0.0)
   dE = similar(E)
-  (; u) = run_values
 
   for i in 1:lastindex(NE)
     NX = []
     ne = 1
-    for dim in 1:n_dims
+    for dim in 1:n_dim
       e = NE[i]
       push!(NX, e)
       ne *= e
@@ -61,7 +114,7 @@ function convergence_test!(E, NE, run_values, n_dims, monta_malha)
 
     malha = monta_malha(NX)
 
-    C = solve_sys_poisson(run_values, malha)
+    C = solve_sys(f, malha, pseudo_a)
 
     E[i], dE[i] = erro_L2(malha, u, C)
   end
